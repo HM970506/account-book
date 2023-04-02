c36066d26bc44a80fcf3661ff62c6c41a96971c2
HM970506 <62527898+HM970506@users.noreply.github.com>
2023-04-02 23:14:27 +0900

    백업

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,51 @@
-import React, { useEffect } from 'react';
-import Classifications from './classifications';
-import getData from './data/getData';
-import FixedExpense from './fixedExpense';
-import Main from './main';
-import Result from './result';
-import ToBuy from './toBuy';
+import React, { useEffect, useRef } from "react";
+import Classifications from "./classifications";
+import getData from "./data/getData";
+import FixedExpense from "./fixedExpense";
+import Main from "./main";
+import Result from "./result";
+import ToBuy from "./toBuy";
 
 function App() {
-  
-  useEffect(()=>{
+  useEffect(() => {
     getData();
-  },[])
+  }, []);
+
+  // let fileReader = new FileReader();
+  // fileReader.onload = () => {
+  //   const textFile = fileReader.result;
+
+  //   if (typeof textFile == "string") {
+  //     const array = textFile.split("\r\n").map((value) => {
+  //       const now = value.split("\t");
+  //       return {
+  //         classification: now[0],
+  //         name: now[1].replace("_", " "),
+  //         value: parseInt(now[2]),
+  //         grade: parseInt(now[3]),
+  //       };
+  //     });
+  //     console.log(JSON.stringify(array));
+  //   }
+  // };
+
+  // const fileread = (e: any) => {
+  //   if (inputRef.current) {
+  //     fileReader.readAsText(e.target.files[0]);
+  //   }
+  // };
+
   return (
     <div className="App">
-      <div><FixedExpense/><ToBuy/></div>
-      <Main/>
-      <div><Result/><Classifications/></div>
+      <div>
+        <FixedExpense />
+        <ToBuy />
+      </div>
+      <Main />
+      <div>
+        <Result />
+        <Classifications />
+      </div>
     </div>
   );
 }
--- src/data/getData.tsx
+++ src/data/getData.tsx
@@ -1,17 +1,18 @@
 import app from "./firebase";
-import {
-  collection,
-  query,
-  onSnapshot,
   getFirestore,
-  getDocs,
-} from "firebase/firestore";
+import { getDatabase, ref, onValue } from "firebase/database";
 
 export default async function getData() {
-  const firestore = getFirestore(app);
+  // const firestore = getFirestore(app);
 
-  const querySnapshot = await getDocs(collection(firestore, "classifications"));
-  querySnapshot.forEach((doc) => {
-    console.log(`${doc.id} => ${doc.data()}`);
+  // const querySnapshot = await getDocs(collection(firestore, "classifications"));
+  // querySnapshot.forEach((doc) => {
+  //   console.log(`${doc.id} => ${doc.data()}`);
+  // });
+  const db = getDatabase(app);
+  const starCountRef = ref(db, "classifications");
+  onValue(starCountRef, (snapshot) => {
+    const data = snapshot.val();
+    console.log(data);
   });
 }
